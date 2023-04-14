4c89cceb2c5111c0c538f612adaba76167224152
Sanyam Chawla <91560018+sanchawla17@users.noreply.github.com>
2023-04-14 14:03:00 +0530

    Youtube Link Problem Fixed

--- home2.php
+++ home2.php
@@ -112,7 +112,7 @@
                     </div>
                     <div class="column">
                         <div class="card">
-                            <iframe id="video" width="560" height="315" src="https://www.youtube.com/embed/D0UnqGm_miA"
+                            <iframe id="video" width="560" height="315" src="https://www.youtube.com/embed/hqRTtaRynSg"
                                     title="YouTube video player"
                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                     allowfullscreen></iframe>
